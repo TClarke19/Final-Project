3d3778cbc439a1382e20e285160f3db1460049a2
Buildertoo <102919008+Buildertoo@users.noreply.github.com>
2023-03-29 16:51:31 -0230

    Initial Commit for Final Project in CP1856

---
+++ BlackJack.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def title():
+    print("\nBLACKJACK!\nBlackjack payout is 3:2")
+
+def menu():
+    print("")
+
+def main():
+    title()
+
+if __name__ == "__main__":
+    main()
