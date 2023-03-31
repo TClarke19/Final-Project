0c3507a320492516fb4d43fafb67a164ab32f446
Buildertoo <102919008+Buildertoo@users.noreply.github.com>
2023-03-31 21:01:43 -0230

    Added some functions

--- BlackJack.py
+++ BlackJack.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
+#import all modules from db
+from db import *
+
+MONEYFILE = "money.txt"
+
 def title():
     print("\nBLACKJACK!\nBlackjack payout is 3:2")
 
-def menu():
-    print("")
-
 def main():
-    title()
+    choice = "y"
+
+    while choice.lower() == "y":
+
+        title()
+        playerMoney = read_money_file(MONEYFILE)
+        print(f"\nMoney; {playerMoney}")
+        playerBetAmount = input("Bet amount: ")
+
+        print("\nDEALER'S SHOW CARD:")
+
+        print("\nYOUR CARDS:")
+
+        choice = input("\nPlay again (y/n): ")
+
+    print("\nCome back soon!\nBye!")
 
 if __name__ == "__main__":
     main()
