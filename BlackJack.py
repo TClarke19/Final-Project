3dfe6fb26a1e8e5de98f59752a776e7fd6d9bab8
Buildertoo <102919008+Buildertoo@users.noreply.github.com>
2023-04-15 16:41:52 -0230

    Edited the main logic strings and added some

--- BlackJack.py
+++ BlackJack.py
@@ -4,7 +4,7 @@
 from db import *
 import random as r
 
-MONEYFILE = "money.txt"
+MONEYFILE = "D:\CNA ASD\Winter 2023\CP1856 Programming with Python\Final Project\money.txt"
 #CARDFILE = "cards.txt"
 
 cardSuit = ["Hearts", "Diamonds", "Clubs", "Spades"]
@@ -29,10 +29,29 @@ def deck_of_cards():
             return deck
             
 def get_top_card(deck):
-    topCard = deck.pop[0]
-    return topCard
+    if deck:
+        topCard = deck.pop(0)
+        return topCard
+    else:
+        return None
+
+def calculate_hand(hand):
+    total = 0
+    for card in hand:
+        if card[1].isdigit():
+            total += int(card[1])
+        elif card[1] in ['Jack', 'Queen', 'King']:
+            total += 10
+        else:
+            if total + 11 > 21:
+                total += 1
+            else:
+                total += 11
+    return total
 
 def main():
+    playerHand = []
+    dealerHand = []
     choice = "y"
 
     while choice.lower() == "y":
@@ -40,19 +59,68 @@ def main():
         try:
             playerMoney = read_money_file(MONEYFILE)
             print(f"\nMoney: {playerMoney}")
-        except:
+        except FileNotFoundError:
             print("\nMoney file not found.")
 
         try:
             playerBetAmount = int(input("Bet amount: "))
             if playerBetAmount < 5 or playerBetAmount > 1000:
                 continue
-        except:
+        except ValueError:
             print("Bet amount has to be greater than $5, and no more than $1000.")
                             
+        deck = deck_of_cards()
+        playerCardOne = get_top_card(deck)
+        playerCardTwo = get_top_card(deck)
+        dealerCard = get_top_card(deck)
+
         print("\nDEALER'S SHOW CARD:")
+        print(dealerCard[0])
 
         print("\nYOUR CARDS:")
+        for card in playerHand:
+            print(card[0])
+
+        while True:
+         choice = input("\nHit or stay? (hit/stay): ")
+         if choice.lower() == "hit":
+             playerHand.append(get_top_card(deck))
+             print("YOUR CARDS:")
+             for card in playerHand:
+                 print(card[0])
+             if calculate_hand(playerHand) > 21:
+                 print("You bust!")
+                 break
+         elif choice.lower() == "stay":
+             print("\nDEALER'S CARDS:")
+             for card in dealerHand:
+                 print(card[0])
+             while calculate_hand(dealerHand) < 17:
+                 dealerHand.append(get_top_card(deck))
+                 print(f"\n{dealerHand[-1][0]}")
+             if calculate_hand(dealerHand) > 21:
+                 print("Dealer bust! You win!")
+                 print(f"\nYour total: {calculate_hand(playerHand)}")
+                 print(f"Dealer's total: {calculate_hand(dealerHand)}")
+                 playerMoney += playerBetAmount * 1.5
+                 write_money_file(MONEYFILE, playerMoney)
+                 break
+             elif calculate_hand(playerHand) > calculate_hand(dealerHand):
+                 print("You win!")
+                 print(f"\nYour total: {calculate_hand(playerHand)}")
+                 print(f"Dealer's total: {calculate_hand(dealerHand)}")
+                 playerMoney += playerBetAmount * 1.5
+                 write_money_file(MONEYFILE, playerMoney)
+                 break
+             elif calculate_hand(playerHand) < calculate_hand(dealerHand):
+                 print("Sorry. You lose.")
+                 print(f"\nYour total: {calculate_hand(playerHand)}")
+                 print(f"Dealer's total: {calculate_hand(dealerHand)}")
+                 break
+             else:
+                 playerMoney += playerBetAmount
+                 write_money_file(MONEYFILE, playerMoney)
+                 break
 
         choice = input("\nPlay again (y/n): ")
 
